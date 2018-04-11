e77dd92d13d51e94b83779e33dc5b710ecf47de2
Mohamed Alie Pussah, II <talk2alie@outlook.com>
2018-04-11 19:14:37 -0400

    Add HelloWorld project

---
+++ Hello_World/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Hello_World
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Jenkins!");
+            Console.Read();
+        }
+    }
+}
